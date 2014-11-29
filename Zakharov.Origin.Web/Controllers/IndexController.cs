407ac5ff1808b0a09ee0a4f1174c4280d37fab74
Anton Zakharov <anton-zakharov@mail.ru>
2014-11-29 21:37:04 +0500

    Добавлены пустые контроллер и представление по умолчанию.

---
+++ Zakharov.Origin.Web/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Zakharov.Origin.Web.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+        public ViewResult Index()
+        {
+            return View();
+        }
+	}
+}
