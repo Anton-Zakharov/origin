7789d79fe2af4f871a69941cbaf8d94e1bbad7ae
Anton Zakharov <anton-zakharov@mail.ru>
2014-11-30 17:25:54 +0500

    Добавлен пустой проект для модульных тестов. Добавлен класс для тестирования родственника.

---
+++ Zakharov.Origin.Test/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zakharov.Origin.Model;
+
+namespace Zakharov.Origin.Test {
+    [TestClass]
+    public class TPerson {
+        [TestMethod]
+        public void insert() {
+            using (COriginContext c_ocContext = new COriginContext()) {
+                CPerson c_peSomeone = new CPerson() { 
+                    Surname = "Иванов" };
+                c_ocContext.Persons.Add(c_peSomeone);
+                int c_iSaved=c_ocContext.SaveChanges();
+                Assert.IsTrue(c_iSaved == 1);
+            }
+        }
+    }
+}
