818fed23138c32d3c5406745d7f406aaa8452ccf
Anton Zakharov <anton-zakharov@mail.ru>
2014-12-09 23:30:44 +0500

    Добавлен проект для служебных классов.

---
+++ Zakharov.Utilities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zakharov.Utilities {
+    /// <summary>
+    /// Сущность
+    /// </summary>
+    public class CEntity {
+        /// <summary>Идентификатор сущности.</summary>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>Дата создания сущности</summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
